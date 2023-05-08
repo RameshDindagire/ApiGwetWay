29a47f4aebc7518be7cc4516ac6f2cc24adf65c4
Ramesh Dindagire <ramesh.d@mobitrail.local>
2023-05-08 14:56:05 +0530

    api getway

---
+++ src/main/java/com/lcdw/getway/ApiGwetWayApplication.java
@@ -0,0 +1,15 @@
+package com.lcdw.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGwetWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGwetWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcdw/getway/ApiGwetWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcdw.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGwetWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
